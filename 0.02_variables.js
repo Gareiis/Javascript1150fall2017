161832ce4ac5430f3c5e8ae20df48e2843e1361d
Thomas <tlontz1@gmail.com>
2017-08-16 12:33:00 -0700

    variables and conditionals

---
+++ 0.02_variables.js
@@ -0,0 +1,24 @@
+// Variables
+
+	//  var name;
+
+	//  name = "Madyson";
+
+	//  var firstName = "Tom";
+	//  var lastName = "Lantz";
+	//  var fullName = firstName + " " + lastName;
+	// console.log(fullName);
+	var myNumber = 0;
+	console.log(myNumber = myNumber + 10);
+	console.log(myNumber = myNumber - 1);
+	console.log(myNumber = myNumber * 9);
+	console.log(myNumber = myNumber / 7);
+
+	var curAge = 22;
+	var maxAge = 85;
+	var perDayAmount = 10;
+	var lifeTimeSupply = (maxAge - curAge) * perDayAmount;
+	console.log(lifeTimeSupply);
+//Variable data types
+
+//Variable scopes
