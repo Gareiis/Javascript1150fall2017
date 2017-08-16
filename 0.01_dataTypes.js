45866e15214dfca595ce081a7008f7eded1fa47b
Thomas <tlontz1@gmail.com>
2017-08-16 10:12:46 -0700

    dataTypes

--- 0.01_dataTypes.js
+++ 0.01_dataTypes.js
@@ -1,26 +1,36 @@
 //DataTypes
 	//strings
-	"What a Wonderful World"
-	'Hello you guys'
-	console.log("Couldn't things get any better");
+		// "What a Wonderful World"
+		// 'Hello you guys'
+		// console.log("Couldn't things get any better");
+		// console.log("jay Jackson"[0].toUpperCase());
+		// console.log("Home"[2].toUpperCase());
+		// console.log("Home"[0] + "Home"[1] + "Home"[2].toUpperCase() + "Home"[3]);
+		
+
+		// var i = "Home"[0];
+		// var num = "two";
+		// var ii = "Home"[1];
+		// var iii = "Home"[2].toUpperCase();
+		// var iiii = "Home"[3];
+
+		// var homes = [i, ii, iii, iiii];
+
+		// console.log(i + ii + iii + iiii);
+
+		// for (var i = 0; i < homes.length; i++) {
+		// 	console.log(homes[i]);
+		// }
+		// console.log(num.valueOf());
 	//numbers
+
+	console.log((2+5));
+
+		// if (10%2) {
+		// 	console.log('true');
+		// }else {
+		// 	console.log('false');
+		// }
 	//booleans
-	console.log("jay Jackson"[0].toUpperCase());
-	console.log("Home"[2].toUpperCase());
-	console.log("Home"[0] + "Home"[1] + "Home"[2].toUpperCase() + "Home"[3]);
-	
-
-	var i = "Home"[0];
-	var num = "two";
-	var ii = "Home"[1];
-	var iii = "Home"[2].toUpperCase();
-	var iiii = "Home"[3];
-
-	var homes = [i, ii, iii, iiii];
-
-	console.log(i + ii + iii + iiii);
-
-	for (var i = 0; i < homes.length; i++) {
-		console.log(homes[i]);
-	}
-	console.log(num.valueOf());
+		console.log(true);
+		console.log(4 === 5);
