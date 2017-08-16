478790aaee87b49aa6ff8d3fcc2a6a958326cacd
Thomas <tlontz1@gmail.com>
2017-08-16 08:53:21 -0700

    second js script

---
+++ 0.01_dataTypes.js
@@ -0,0 +1,26 @@
+//DataTypes
+	//strings
+	"What a Wonderful World"
+	'Hello you guys'
+	console.log("Couldn't things get any better");
+	//numbers
+	//booleans
+	console.log("jay Jackson"[0].toUpperCase());
+	console.log("Home"[2].toUpperCase());
+	console.log("Home"[0] + "Home"[1] + "Home"[2].toUpperCase() + "Home"[3]);
+	
+
+	var i = "Home"[0];
+	var num = "two";
+	var ii = "Home"[1];
+	var iii = "Home"[2].toUpperCase();
+	var iiii = "Home"[3];
+
+	var homes = [i, ii, iii, iiii];
+
+	console.log(i + ii + iii + iiii);
+
+	for (var i = 0; i < homes.length; i++) {
+		console.log(homes[i]);
+	}
+	console.log(num.valueOf());
